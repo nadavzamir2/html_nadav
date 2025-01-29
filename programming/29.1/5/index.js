aa50db7adb7ca5a63d604f9a2205897fb184135b
galamo <galamouyal88@gmail.com>
2025-01-29 21:26:36 +0200

    a

--- programming/29.1/5/index.js
+++ programming/29.1/5/index.js
@@ -5,6 +5,10 @@ const MODEL = "octavia"
 const carType = prompt("Enter the car type?")
 const carColor = prompt("Enter the car color?")
 const carModel = prompt("Enter the car Model?")
+const price = prompt("Enter the car price?")
+
+const priceNumber = Number(price)
+console.log(priceNumber)
 
 // how to improve this input validation? 
 // if (type carType !== "" && carColor !== "" && carModel !== "") {
@@ -12,11 +16,13 @@ const carModel = prompt("Enter the car Model?")
 if (typeof carType === "string" && carType.length > 0 &&
     typeof carColor === "string" && carColor.length > 0 &&
     typeof carModel === "string" && carModel.length > 0
+
 ) {
     // throw new Error("Missing car type")
     if (carType.toLowerCase() === TYPE
         && carColor.toUpperCase() === COLOR
-        && carModel.toLowerCase() === MODEL) {
+        && carModel.toLowerCase() === MODEL
+        && priceNumber < 1000) {
         console.log(`I found the Car => ${TYPE}`)
     } else {
         console.log("Dont need this car!!")
