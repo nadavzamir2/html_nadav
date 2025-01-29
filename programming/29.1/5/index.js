87ee68a5cce49423d5976802b912ce1f5ff187e4
galamo <galamouyal88@gmail.com>
2025-01-29 21:06:32 +0200

    a

--- programming/29.1/5/index.js
+++ programming/29.1/5/index.js
@@ -1,16 +1,31 @@
+const TYPE = "skoda"
+const COLOR = "WHITE"
+const MODEL = "octavia"
+
 const carType = prompt("Enter the car type?")
 const carColor = prompt("Enter the car color?")
 const carModel = prompt("Enter the car Model?")
 
-const TYPE = "skoda"
-const MODEL = "octavia"
-const COLOR = "WHITE"
-
-if (carType.toLowerCase() === TYPE
-    && carColor.toUpperCase() === COLOR
-    && carModel.toLowerCase() === MODEL) {
-    console.log(`I found the Car => ${TYPE}`)
+// how to improve this input validation? 
+// if (type carType !== "" && carColor !== "" && carModel !== "") {
+// new input validation: 
+if (typeof carType === "string" && carType.length > 0 &&
+    typeof carColor === "string" && carColor.length > 0 &&
+    typeof carModel === "string" && carModel.length > 0
+) {
+    // throw new Error("Missing car type")
+    if (carType.toLowerCase() === TYPE
+        && carColor.toUpperCase() === COLOR
+        && carModel.toLowerCase() === MODEL) {
+        console.log(`I found the Car => ${TYPE}`)
+    } else {
+        console.log("Dont need this car!!")
+    }
 } else {
-    console.log("Dont need this car!!")
+    console.log("Input validation error, missing values, Talya made us orange cake, please join")
 }
+
+
+
+
 
